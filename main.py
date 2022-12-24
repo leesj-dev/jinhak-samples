3c85e4108d9b5786db353000378da1a9afb780b6
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-12-25 00:42:12 +0900

    예쁘게 정리

--- main.py
+++ main.py
@@ -24,9 +24,7 @@ chrome_options.add_argument("headless")
 chrome_options.add_argument("window-size=1920x1080")
 chrome_options.add_argument("--start-maximized")
 """
-driver = webdriver.Chrome(
-    service=Service(ChromeDriverManager().install()), options=chrome_options
-)
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
 ## 링크 접속
 driver.get("https://hijinhak.jinhak.com/SAT/J1Apply/J1MyApplyList.aspx?LeftTab=1")
@@ -42,10 +40,9 @@ def digit_to_int(classes: list, type_of_class: str) -> float:
     num = ""
     for item in zip(*[classes[i : i + length] for i in range(0, len(classes), length)]):
         num += convert_dict_2[item]
-
     return float(num)
 
-
+## 군별 크롤링
 def scrape_group(group_id):
     driver.switch_to.window(driver.window_handles[0])  # 메인창으로 이동
     driver.execute_script("parent.ReportOpenLast('3', '" + group_id + "', 'TOT');")
@@ -55,20 +52,14 @@ def scrape_group(group_id):
     driver.switch_to.frame(driver.find_element(By.ID, "ifrmGraph"))  # iframe 내에 있으므로
 
     # css 정보 스크레이핑
-    digital_css = driver.find_element(By.XPATH, "/html/body/style").get_attribute(
-        "innerHTML"
-    )
+    digital_css = driver.find_element(By.XPATH, "/html/body/style").get_attribute("innerHTML")
 
     for item in convert.digital_dict.keys():
         if item in digital_css:
             digital_info = item
 
     # 전처리
-    css_info = [
-        re.split("\s|:", item[2:], 1)
-        for item in digital_css.split("." + digital_info)
-        if item != " "
-    ]
+    css_info = [re.split("\s|:", item[2:], 1) for item in digital_css.split("." + digital_info) if item != " "]
 
     # 마지막 문자 공백 시 제거
     for item in css_info:
@@ -92,23 +83,21 @@ def scrape_group(group_id):
     # [1::2]는 중복방지를 위해 그 중 짝수 번째(bottom)만 출력
     scores = driver.find_elements(By.CLASS_NAME, digital_info)[1::2]
 
-    ## HTML 파싱
+    # HTML 파싱
     for item in scores:
         soup = BeautifulSoup(item.get_attribute("innerHTML"), "html.parser")
-        classes = [
-            value for item in soup.find_all(class_=True) for value in item["class"]
-        ]
+        classes = [value for item in soup.find_all(class_=True) for value in item["class"]]
         convert_dict_1 = convert.digital_dict[digital_info][0]
         classes_std = []
 
-        # classes 내의 각 class_name에 대하여 -> css 정보로 변환 -> 다시 Tx 표준형으로 변환 -> 해당되는 숫자로 변환
+        # classes 내의 각 class_name에 대하여 -> css 정보로 변환 -> 다시 표준형으로 변환 -> 해당되는 숫자로 변환
         for class_name in classes:
             try:
                 classes_std.append(convert_dict_1[css_dict[class_name]])
             except:  # 아무것도 없는 거는 애초에 CSS 정의가 안 되어있으므로 * 처리
                 classes_std.append("*")
         try:
-            print("{:.2f}".format(digit_to_int(classes_std, digital_info)))  # 소숫점 둘째 자리
+            print("{:.2f}".format(digit_to_int(classes_std, digital_info)))  # 소숫점 둘째 자리까지 출력
         except:
             print("Error")
 
