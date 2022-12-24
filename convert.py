68cbb8c242df29f1aa160122dfabd703612b9103
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-12-25 00:39:10 +0900

    digital, digitalM 대응 완료

---
+++ convert.py
@@ -0,0 +1,200 @@
+digitalT_std = {
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "T1",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "T2",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+    ): "T3",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "T4",
+    ("after { right: 2px; bottom: 0; width: 2px; height: 2px; }",): "T5",
+    ("em:after { top: 0; bottom: 0; left: 0; width: 1px; }",): "T6",
+    ("after { top: 0; right: 0; bottom: 0; width: 1px; }",): "T7",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "T8",
+    (
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+    ): "T9",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "T10",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+    ): "T11",
+}
+
+digits_digitalT = {
+    ("T7", "T7"): "1",
+    ("T3", "T2"): "2",
+    ("T3", "T11"): "3",
+    ("T9", "T3"): "4",
+    ("T8", "T11"): "5",
+    ("T6", "T1"): "6",
+    ("T3", "T7"): "7",
+    ("T10", "T1"): "8",
+    ("T10", "T3"): "9",
+    ("T10", "T4"): "0",
+    ("*", "T5"): ".",
+}
+
+digitalM_std = {
+    (
+        "after { top: 50%; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M1",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "M2",
+    (
+        "after { top: 0; right: 0; bottom: 50%; width: 1px; }",
+        "em:after { top: 50%; bottom: 0%; left: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M3",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M4",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "M5",
+    ("after { right: 2px; bottom: 0; width: 2px; height: 2px; }",): "M6",
+    ("em:after { top: 0; bottom: 0; left: 0; width: 1px; }",): "M7",
+    (
+        "after { top: 50%; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 50%; left: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M8",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "M9",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M10",
+    ("after { top: 0; right: 0; bottom: 0; width: 1px; }",): "M11",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 50%; left: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 35%; left: 0; height: 1px; }",
+    ): "M12",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+    ): "M13",
+    (
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "M14",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+    ): "M15",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "M16",
+}
+
+digits_digitalM = {
+    ("M11", "M11", "M11"): "1",
+    ("M15", "M3", "M14"): "2",
+    ("M15", "M4", "M13"): "3",
+    ("M16", "M12", "M11"): "4",
+    ("M9", "M8", "M13"): "5",
+    ("M7", "M1", "M5"): "6",
+    ("M15", "M11", "M11"): "7",
+    ("M2", "M10", "M5"): "8",
+    ("M2", "M12", "M11"): "9",
+    ("M2", "M16", "M5"): "0",
+    ("*", "*", "M6"): ".",
+}
+
+
+digital_std = {
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+    ): "S1",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px;}",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "S2",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+    ): "S3",
+    ("after { right: 2px; bottom: 0; width: 2px; height: 2px;}",): "S4",
+    (
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px;}",
+    ): "S5",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px;}",
+    ): "S6",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px;}",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+    ): "S7",
+    (
+        "before { top: 0; left: 0; right: 0; height: 1px; }",
+        "after { top: 0; right: 0; bottom: 0; width: 1px; }",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px;}",
+    ): "S8",
+    (
+        "after { top: 0; right: 0; bottom: 0; width: 1px;}",
+        "em:before { right: 0; bottom: 0; left: 0; height: 1px; }",
+        "em:after { top: 0; bottom: 0; left: 0; width: 1px; }",
+    ): "S9",
+    ("after { top: 0; right: 0; bottom: 0; width: 1px;}",): "S10",
+}
+
+digits_digital = {
+    ("S10", "S10"): "1",
+    ("S7", "S5"): "2",
+    ("S7", "S3"): "3",
+    ("S9", "S10"): "4",
+    ("S6", "S3"): "5",
+    ("S5", "S9"): "6",
+    ("S1", "S10"): "7",
+    ("S8", "S9"): "8",
+    ("S8", "S10"): "9",
+    ("S2", "S9"): "0",
+    ("*", "S4"): ".",
+}
+
+digital_dict = {
+    "digital": [digital_std, digits_digital, 2],
+    "digitalT": [digitalT_std, digits_digitalT, 2],
+    "digitalM": [digitalM_std, digits_digitalM, 3],
+}
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 import os
 import time
 import re
-import convert_tables
+import convert
 
 
 ## 초기설정
@@ -37,87 +37,86 @@ driver.implicitly_wait(5)
 
 ## list의 문자들을 int로 변환하는 함수
 def digit_to_int(classes: list, type_of_class: str) -> float:
-    length = len(classes) // 2
+    length = len(classes) // convert.digital_dict[type_of_class][2]
+    convert_dict_2 = convert.digital_dict[type_of_class][1]
     num = ""
-    if type_of_class == "digitalT":
-        dictionary = convert_tables.digits_digitalT
-    else:
-        print("Error")
+    for item in zip(*[classes[i : i + length] for i in range(0, len(classes), length)]):
+        num += convert_dict_2[item]
 
-    # 아래 split문은 패턴이 2가지일 때만 성립하며, 3일 때도 있으므로(digitalM) 추후 다른 방법을 강구해보겠음
-    for item in zip(classes[:length], classes[length:]):
-        num += dictionary[item]
     return float(num)
 
 
-### 가군
-driver.execute_script("parent.ReportOpenLast('3', '2C31461515AD59D5E40', 'TOT');")  # 가군
-driver.switch_to.window(driver.window_handles[1])  # 팝업창으로 이동
-driver.execute_script("window.scrollTo(0,1400)")  # 스크롤 내리기
-time.sleep(5)
-driver.switch_to.frame(driver.find_element(By.ID, "ifrmGraph"))  # iframe 내에 있으므로
+def scrape_group(group_id):
+    driver.switch_to.window(driver.window_handles[0])  # 메인창으로 이동
+    driver.execute_script("parent.ReportOpenLast('3', '" + group_id + "', 'TOT');")
+    driver.switch_to.window(driver.window_handles[1])  # 팝업창으로 이동
+    driver.execute_script("window.scrollTo(0,1500)")  # 스크롤 내리기
+    time.sleep(5)  # 내부 iframe 로딩
+    driver.switch_to.frame(driver.find_element(By.ID, "ifrmGraph"))  # iframe 내에 있으므로
 
-## 점수 스크레이핑
-digitalT_scrape = driver.find_element(By.XPATH, "/html/body/style").get_attribute(
-    "innerHTML"
-)
+    # css 정보 스크레이핑
+    digital_css = driver.find_element(By.XPATH, "/html/body/style").get_attribute(
+        "innerHTML"
+    )
 
-# 점수 전처리
-css_info = [
-    re.split("\s|:", item[2:], 1)
-    for item in digitalT_scrape.split(".digitalT")
-    if item != " "
-]
+    for item in convert.digital_dict.keys():
+        if item in digital_css:
+            digital_info = item
 
-for item in css_info:  # 마지막 문자 공백 시 제거
-    if item[1][-1] == " ":
-        item[1] = item[1][:-1]
+    # 전처리
+    css_info = [
+        re.split("\s|:", item[2:], 1)
+        for item in digital_css.split("." + digital_info)
+        if item != " "
+    ]
 
-key, val = map(list, zip(*css_info))
-css_dict = {}
-for i in range(len(key) - 1):
-    if key[i] != key[i + 1]:
-        css_dict[key[i]] = ()
-css_dict[key[len(key) - 1]] = ()
-for i in range(len(key)):
-    css_dict[key[i]] += (val[i],)
-# css_dict = {y: x for x, y in css_dict.items()} -- 변환 용도 (사용 금지)
+    # 마지막 문자 공백 시 제거
+    for item in css_info:
+        if item[1][-1] == " ":
+            item[1] = item[1][:-1]
 
-## 과목 스크레이핑 (좀 있다가 할 것)
-# subjects = driver.find_elements(By.XPATH, '//*[@id="DivA"]/div/*/div[2]/p[1]/span/span')
-# print(subjects)
-scores = driver.find_elements(By.CLASS_NAME, "digitalT")
+    key, val = map(list, zip(*css_info))
+    css_dict = {}
+    for i in range(len(key) - 1):
+        if key[i] != key[i + 1]:
+            css_dict[key[i]] = ()
+    css_dict[key[len(key) - 1]] = ()
+    for i in range(len(key)):
+        css_dict[key[i]] += (val[i],)
+    # print({y: x for x, y in css_dict.items()})  # -- 변환 용도 (사용 금지)
 
-# HTML 파싱
-for item in scores:
-    soup = BeautifulSoup(item.get_attribute("innerHTML"), "html.parser")
-    classes = [value for item in soup.find_all(class_=True) for value in item["class"]]
-    classes_std = []
+    ## 과목 스크레이핑 (좀 있다가 할 것)
+    # subjects = driver.find_elements(By.XPATH, '//*[@id="DivA"]/div/*/div[2]/p[1]/span/span')
+    # print(subjects)
 
-    # classes 내의 각 class_name에 대하여 -> css 정보로 변환 -> 다시 Tx 표준형으로 변환 -> 해당되는 숫자로 변환
-    for class_name in classes:
+    # [1::2]는 중복방지를 위해 그 중 짝수 번째(bottom)만 출력
+    scores = driver.find_elements(By.CLASS_NAME, digital_info)[1::2]
+
+    ## HTML 파싱
+    for item in scores:
+        soup = BeautifulSoup(item.get_attribute("innerHTML"), "html.parser")
+        classes = [
+            value for item in soup.find_all(class_=True) for value in item["class"]
+        ]
+        convert_dict_1 = convert.digital_dict[digital_info][0]
+        classes_std = []
+
+        # classes 내의 각 class_name에 대하여 -> css 정보로 변환 -> 다시 Tx 표준형으로 변환 -> 해당되는 숫자로 변환
+        for class_name in classes:
+            try:
+                classes_std.append(convert_dict_1[css_dict[class_name]])
+            except:  # 아무것도 없는 거는 애초에 CSS 정의가 안 되어있으므로 * 처리
+                classes_std.append("*")
         try:
-            classes_std.append(convert_tables.digitalT_std[css_dict[class_name]])
-        except:  # 아무것도 없는 거는 애초에 CSS 정의가 안 되어있음
-            classes_std.append("*")
-    try:
-        print(digit_to_int(classes_std, "digitalT"))
-    except:
-        print("Error")
+            print("{:.2f}".format(digit_to_int(classes_std, digital_info)))  # 소숫점 둘째 자리
+        except:
+            print("Error")
+
+    time.sleep(3)
+
+
+group_ids = ["2C31461515AD59D5E40", "2C31461515AD59D5E40", "2C313705174A9FE40AA"]
+scrape_group(group_ids[2])
 
 # 디버깅 용도
 time.sleep(10000)
-
-"""
-## 나군
-driver.switch_to.window(driver.window_handles[0])
-driver.execute_script("parent.ReportOpenLast('3', '23R141591F09D90464D', 'TOT');")  # 나군
-driver.switch_to.window(driver.window_handles[1])
-time.sleep(10)
-
-## 다군
-driver.switch_to.window(driver.window_handles[0])
-driver.execute_script("parent.ReportOpenLast('3', '23R13705168452A594F', 'TOT');")  # 다군
-driver.switch_to.window(driver.window_handles[1])
-time.sleep(10000)
-"""
