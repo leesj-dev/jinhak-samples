8040d8a9d42d6ca10f97811f07701cb25182e960
leesj-dev <104193215+leesj-dev@users.noreply.github.com>
2022-12-25 01:02:08 +0900

    type hinting 추가

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ driver.implicitly_wait(5)
 
 ## list의 문자들을 int로 변환하는 함수
 def digit_to_int(classes: list, type_of_class: str) -> float:
-    length = len(classes) // convert.digital_dict[type_of_class][2]
-    convert_dict_2 = convert.digital_dict[type_of_class][1]
-    num = ""
+    length: int = len(classes) // convert.digital_dict[type_of_class][2]
+    convert_dict_2: dict = convert.digital_dict[type_of_class][1]
+    num: str = ""
     for item in zip(*[classes[i : i + length] for i in range(0, len(classes), length)]):
         num += convert_dict_2[item]
     return float(num)
@@ -52,13 +52,14 @@ def scrape_group(group_id):
     driver.switch_to.frame(driver.find_element(By.ID, "ifrmGraph"))  # iframe 내에 있으므로
 
     # css 정보 스크레이핑
-    digital_css = driver.find_element(By.XPATH, "/html/body/style").get_attribute("innerHTML")
+    digital_css: str = driver.find_element(By.XPATH, "/html/body/style").get_attribute("innerHTML")
 
+    # digital / digitalT / digitalM 중 어느 것인지 판별
     for item in convert.digital_dict.keys():
         if item in digital_css:
             digital_info = item
 
-    # 전처리
+    # 아래부터 계속 전처리
     css_info = [re.split("\s|:", item[2:], 1) for item in digital_css.split("." + digital_info) if item != " "]
 
     # 마지막 문자 공백 시 제거
@@ -81,13 +82,13 @@ def scrape_group(group_id):
     # print(subjects)
 
     # [1::2]는 중복방지를 위해 그 중 짝수 번째(bottom)만 출력
-    scores = driver.find_elements(By.CLASS_NAME, digital_info)[1::2]
+    scores: list = driver.find_elements(By.CLASS_NAME, digital_info)[1::2]
 
     # HTML 파싱
     for item in scores:
         soup = BeautifulSoup(item.get_attribute("innerHTML"), "html.parser")
         classes = [value for item in soup.find_all(class_=True) for value in item["class"]]
-        convert_dict_1 = convert.digital_dict[digital_info][0]
+        convert_dict_1: dict = convert.digital_dict[digital_info][0]
         classes_std = []
 
         # classes 내의 각 class_name에 대하여 -> css 정보로 변환 -> 다시 표준형으로 변환 -> 해당되는 숫자로 변환
